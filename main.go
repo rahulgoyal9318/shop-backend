153f2e92d656d40be10ee9a1f8b63ac2cedf3e1d
Rahul Goyal <rahul.goyal9318@gmail.com>
2026-05-08 09:27:54 +0530

    http server

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Rahul"))
+}
